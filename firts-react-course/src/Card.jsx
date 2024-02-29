eee9017c94841759acb7f06cc59ae836422fd82f
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-02-29 04:50:41 +0100

    added gitignore

--- firts-react-course/src/Card.jsx
+++ firts-react-course/src/Card.jsx
@@ -4,7 +4,7 @@ function Card() {
     return (
         <div className='card'>
             <img className='card-image' src='https://via.placeholder.com/150' alt="profile picture"/>
-            <h2>Borge</h2>
+            <h2 className='card-title'>Borge</h2>
             <p>I Am a Developer</p>
         </div>
     )
