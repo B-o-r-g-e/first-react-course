aaa80860e15a880476b6f9e3ffb4af2aef691e7c
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-03-01 00:03:09 +0100

    .

--- firts-react-course/src/Components/Blog.jsx
+++ firts-react-course/src/Components/Blog.jsx
@@ -2,7 +2,7 @@ import {useState} from "react";
 import BlogList from "./BlogList.jsx";
 
 const Blog = () => {
-    const [blogs, setBlogs] = useState([
+    const [blogs, /*setBlogs*/] = useState([
         {title: 'My new blog', body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores iure nihil, nulla odio provident quasi repellat sit soluta voluptate voluptatem!', author: 'James', id: 1},
         {title: 'My new website', body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores iure nihil, nulla odio provident quasi repellat sit soluta voluptate voluptatem!', author: 'Joy', id: 2},
         {title: 'My new cup', body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores iure nihil, nulla odio provident quasi repellat sit soluta voluptate voluptatem!', author: 'Crane', id: 3}
