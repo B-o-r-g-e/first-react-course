6a6a2e62a7e520744dc6ef54c6f6982fd92f9b9b
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-03-01 00:06:22 +0100

    .

--- firts-react-course/src/Components/BlogList.jsx
+++ firts-react-course/src/Components/BlogList.jsx
@@ -1,6 +1,7 @@
-const BlogList = (props) => {
-    const blogs = props.blogs
-    const title = props.title
+// const BlogList = (props) => {
+const BlogList = ({blogs, title}) => {
+    // const blogs = props.blogs
+    // const title = props.title
 
     return (
         <div className="blog-list">
