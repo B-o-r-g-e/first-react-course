245423f852c064156f940056d9146ac0e6a17780
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-02-29 21:51:11 +0100

    .

--- firts-react-course/src/Components/Body.jsx
+++ firts-react-course/src/Components/Body.jsx
@@ -1,5 +1,15 @@
-const Body = () => {
+import {useState} from "react";
 
+const Body = () => {
+    const [name, setName] = useState('mario')
+
+    const handler = () => {
+
+    }
+
+    return (
+        <button onClick={handler}>Click me again</button>
+    )
 }
 
 export default Body
