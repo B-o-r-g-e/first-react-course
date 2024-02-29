67e6245441a00a5f999910889d4892228c9fa15a
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-02-29 22:29:53 +0100

    .

--- firts-react-course/src/Components/Body.jsx
+++ firts-react-course/src/Components/Body.jsx
@@ -2,14 +2,17 @@ import {useState} from "react";
 
 const Body = () => {
     const [name, setName] = useState('Mario')
+    const [age, setAge] = useState(28)
 
     const handler = () => {
         setName('James')
+        setAge(40)
+        console.log(name)
     }
 
     return (
         <>
-            <p>{name}</p>
+            <p>{name} is {age} years old</p>
             <button onClick={handler}>Click me again</button>
         </>
 
