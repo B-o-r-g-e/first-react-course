df073700e29d0ed81ea2ef54d1da9a77e5378fca
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-02-29 04:58:18 +0100

    .

--- firts-react-course/src/App.jsx
+++ firts-react-course/src/App.jsx
@@ -1,7 +1,8 @@
 // import Header from "./Header.jsx";
 // import Footer from "./Footer.jsx";
 // import Food from "./Food.jsx";
-import Card from "./Card.jsx";
+// import Card from "./Card.jsx";
+import Button from "./Button.jsx";
 
 function App() {
     return (
@@ -10,8 +11,9 @@ function App() {
             {/*<Food/>*/}
             {/*<Food/>*/}
             {/*<Footer/>*/}
-            <Card/>
-            <Card/>
+            {/*<Card/>*/}
+            {/*<Card/>*/}
+            <Button/>
         </>
     );
 }
