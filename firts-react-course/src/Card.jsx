6f81e5b81f8fd0dd4a8f2516888d8238cf6bb967
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-02-29 04:35:48 +0100

    added gitignore

--- firts-react-course/src/Card.jsx
+++ firts-react-course/src/Card.jsx
@@ -1,7 +1,9 @@
+import profilePic from './assets/dp.jpeg'
+
 function Card() {
     return (
         <div className='card'>
-            <img src="" alt="#"/>
+            <img src="" alt="profile picture"/>
             <h2>Borge</h2>
             <p>I Am a Developer</p>
         </div>
