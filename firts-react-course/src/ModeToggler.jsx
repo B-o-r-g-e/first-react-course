4c256351255338dbda8039bb4b03e955487f586d
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-02-29 10:51:34 +0100

    .

--- firts-react-course/src/ModeToggler.jsx
+++ firts-react-course/src/ModeToggler.jsx
@@ -1,11 +1,19 @@
 function ModeToggler() {
-    const darkModeOn = true
+    let darkModeOn = true
     const darkMode = <h1>Dark mode is On</h1>
     const lightMode = <h1>Light mode is On</h1>
+    
+    function handler() {
+        darkModeOn = !darkModeOn
+
+    }
 
     return (
         <div>
             {darkModeOn ? darkMode : lightMode}
+            <button className={'button'} onClick={handler}>
+                Click me
+            </button>
         </div>
     )
 }
