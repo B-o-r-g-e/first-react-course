c13a893808e4c436b99fe58b158822ae9ff5dfc5
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-02-29 04:26:38 +0100

    added gitignore

---
+++ firts-react-course/src/Card.jsx
@@ -0,0 +1,11 @@
+function Card() {
+    return (
+        <div className={card}>
+            <img src="" alt="#"/>
+            <h2></h2>
+            <p></p>
+        </div>
+    )
+}
+
+export default Card
