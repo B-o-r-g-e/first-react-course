9012fa2f51f4ffa57c02db7cf9bffba4670ab0a3
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-03-01 01:59:28 +0100

    .

--- firts-react-course/src/Components/Blog.jsx
+++ firts-react-course/src/Components/Blog.jsx
@@ -4,21 +4,25 @@ import BlogList from "./BlogList.jsx";
 const Blog = () => {
     const [blogs, setBlogs] = useState(null)
 
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id !== id)
-        setBlogs(newBlogs)
-    }
+    // const handleDelete = (id) => {
+    //     const newBlogs = blogs.filter(blog => blog.id !== id)
+    //     setBlogs(newBlogs)
+    // }
 
     useEffect(() => {
         fetch('http://localhost:8000/blogs')
             .then(res => {
                 return res.json()
             })
+            .then(data => {
+                console.log(data)
+                setBlogs(data)
+            })
     }, [])
 
     return (
         <div className="home">
-            <BlogList blogs = {blogs} title = 'All Blogs' handleDelete={handleDelete} />
+            {blogs && <BlogList blogs={blogs} title='All Blogs' handleDelete={handleDelete}/>}
 
 
             {/*<BlogList blogs = {blogs*/}
