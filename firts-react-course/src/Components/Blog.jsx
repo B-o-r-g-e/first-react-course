ae115325c0d75647e0f21225a6cd2cab987607af
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-03-01 02:51:41 +0100

    .

--- firts-react-course/src/Components/Blog.jsx
+++ firts-react-course/src/Components/Blog.jsx
@@ -2,29 +2,10 @@ import {useEffect, useState} from "react";
 import BlogList from "./BlogList.jsx";
 
 const Blog = () => {
-    const [blogs, setBlogs] = useState(null)
-    const [isPending, setIsPending] = useState(true)
-    const [error, setError] = useState(null)
 
 
-    useEffect(() => {
-        fetch('http://localhost:8000/blogs')
-            .then(res => {
-                if (!res.ok) {
-                    throw Error('could not fetch')
-                }
-                return res.json()
-            })
-            .then(data => {
-                setBlogs(data)
-                setIsPending(false)
-                setError(null)
-            })
-            .catch(err => {
-                setIsPending(false)
-                setError(err.message)
-            })
-    }, [])
+
+
 
     return (
         <div className="home">
