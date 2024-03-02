d6cc54781834c3dd8743ebaee91ed78cb5a594a9
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-03-02 05:17:38 +0100

    .

--- firts-react-course/src/App.jsx
+++ firts-react-course/src/App.jsx
@@ -1,7 +1,7 @@
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
 
-// import Header from "./Header.jsx";
+import Header from "./Components/Header.jsx";
 // import Footer from "./Footer.jsx";
 // import Food from "./Food.jsx";
 // import Card from "./Card.jsx";
@@ -17,7 +17,7 @@ function App() {
     return (
         <Router>
             <>
-                {/*<Header/>*/}
+                <Header/>
                 {/*<Food/>*/}
                 {/*<Food/>*/}
                 {/*<Footer/>*/}
@@ -31,7 +31,7 @@ function App() {
                 {/*<Student/>*/}
                 {/*<ModeToggler />*/}
                 {/*<Body />*/}
-                <Blog />
+                {/*<Blog />*/}
                 <div className={'content'}>
                     <Switch>
                         <Route path={'/'}>
