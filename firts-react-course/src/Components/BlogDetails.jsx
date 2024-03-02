b5cb975e9d296408ec5201fbeaf994a98515a0b2
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-03-02 06:48:03 +0100

    .

--- firts-react-course/src/Components/BlogDetails.jsx
+++ firts-react-course/src/Components/BlogDetails.jsx
@@ -1,11 +1,18 @@
 import {useParams} from "react-router-dom";
+import useFetch from "./UseFetch.jsx";
 
 const BlogDetails = () => {
     const { id } = useParams()
-
+    const { data: blog, error, isPending} = useFetch('http://localhost:5173/blogs/' + id)
     return (
         <div className="blog-details">
-            <h2>Blog Details - { id }</h2>
+            {isPending && <div>Loading...</div>}
+            {error && <div>{ error }</div>}
+            {blog && (
+                <article>
+                    <h2>{blog.title}</h2>
+                </article>
+            )}
         </div>
     )
 }
