3e07b1bff3e52496501145a6d39115956be19f83
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-03-04 13:37:56 +0100

    0

--- firts-react-course/src/Components/Create.jsx
+++ firts-react-course/src/Components/Create.jsx
@@ -3,6 +3,7 @@ import {useState} from "react";
 const Create = () => {
     const [title, setTitle] = useState('')
     const [body, setBody] = useState('')
+    const [author, setAuthor] = useState('')
 
     return (
         <div className="create">
@@ -22,13 +23,17 @@ const Create = () => {
                     onChange={(e) => {setBody(e.target.value)}}
                 ></textarea>
                 <label>Blog author</label>
-                <select>
+                <select
+                    value={author}
+                    onChange={(e) => {setAuthor(e.target.value)}}
+                >
                     <option value="James">James</option>
                     <option name="Joy">Joy</option>
                 </select>
                 <button>Add Blog</button>
                 <p>{title}</p>
                 <p>{body}</p>
+                <p>{author}</p>
             </form>
         </div>
     )
