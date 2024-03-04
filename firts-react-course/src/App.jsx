31146eacf3772d6ba50ae43394a2da37e3bd4daf
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-03-04 14:38:34 +0100

    .

--- firts-react-course/src/App.jsx
+++ firts-react-course/src/App.jsx
@@ -14,6 +14,7 @@ import Header from "./Components/Header.jsx";
 import Blog from "./Components/Blog.jsx";
 import Create from "./Components/Create.jsx";
 import BlogDetails from "./Components/BlogDetails.jsx";
+import NotFound from "./Components/NotFound.jsx";
 
 function App() {
     return (
@@ -44,6 +45,9 @@ function App() {
                         </Route>
                         <Route path={'/blogs/:id'}>
                             <BlogDetails />
+                        </Route>
+                        <Route path={'*'}>
+                            <NotFound />
                         </Route>
                     </Switch>
                 </div>
