dc06826d77323bb123c4e5d5b652e9c273ea4392
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-03-06 22:47:45 +0100

    .

---
+++ firts-react-course/src/Components/Fruits.jsx
@@ -0,0 +1,9 @@
+const Fruits = [
+    {name: 'apple', calories: 40},
+    {name: 'Banana', calories: 16},
+    {name: 'apple', calories: 10},
+    {name: 'apple', calories: 30},
+    {name: 'apple', calories: 90}
+]
+
+export default Fruits
--- firts-react-course/src/Components/List.jsx
+++ firts-react-course/src/Components/List.jsx
@@ -1,12 +1,3 @@
-const fruits = [
-    {name: 'apple', calories: 40},
-    {name: 'Banana', calories: 16},
-    {name: 'apple', calories: 10},
-    {name: 'apple', calories: 30},
-    {name: 'apple', calories: 90}
-
-]
-
 function List(props) {
 
 }
