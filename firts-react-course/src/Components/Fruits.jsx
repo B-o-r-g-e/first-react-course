e28046245bf6fe6530b4ef09a61f98f9b1f599ba
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-03-06 22:50:28 +0100

    .

--- firts-react-course/src/Components/Fruits.jsx
+++ firts-react-course/src/Components/Fruits.jsx
@@ -1,9 +1,9 @@
 const Fruits = [
-    {name: 'apple', calories: 40},
-    {name: 'Banana', calories: 16},
-    {name: 'apple', calories: 10},
-    {name: 'apple', calories: 30},
-    {name: 'apple', calories: 90}
+    {name: 'apple', calories: 40, id: 1},
+    {name: 'Banana', calories: 16, id: 2},
+    {name: 'apple', calories: 10, id: 3},
+    {name: 'apple', calories: 30, id: 4},
+    {name: 'apple', calories: 90, id: 5}
 ]
 
 export default Fruits
