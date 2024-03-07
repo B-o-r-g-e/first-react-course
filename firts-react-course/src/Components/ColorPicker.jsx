509a13ccb8c381fb2ac175078af5d89f901cad17
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-03-07 07:26:58 +0100

    .

--- firts-react-course/src/Components/ColorPicker.jsx
+++ firts-react-course/src/Components/ColorPicker.jsx
@@ -3,12 +3,14 @@ import {useState} from "react";
 function ColorPicker() {
     const [color, setColor] = useState('#FFFFFF')
 
-    return (<div className={'color-picker-container'} style={{background: color}}>
-        <div className="color-display">
-            <p>Selected Color: {color}</p>
+    return (
+        <div className={'color-picker-container'} style={{background: color}}>
+            <div className="color-display">
+                <p>Selected Color: {color}</p>
+            </div>
+            <input type="color" value={color}/>
         </div>
-        <input type="color" value={color}/>
-    </div>)
+    )
 }
 
 export default ColorPicker
