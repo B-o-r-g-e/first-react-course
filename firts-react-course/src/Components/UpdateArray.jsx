ab33c2f2c30dd00c09ff70efd17f50ba985cb137
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-03-07 10:31:40 +0100

    .

--- firts-react-course/src/Components/UpdateArray.jsx
+++ firts-react-course/src/Components/UpdateArray.jsx
@@ -1,7 +1,17 @@
+import {useState} from "react";
+import list from "./List.jsx";
+
 const UpdateArray = () => {
+    const [foods, setFoods] = useState(['Apple', 'Banana', 'Orange'])
+
     return (
         <div>
-
+            <h2>Lists of Foods</h2>
+            <ul>
+                {foods.map((food, index) =>
+                    <li key={index}>{food}</li>
+                )}
+            </ul>
         </div>
     )
 }
