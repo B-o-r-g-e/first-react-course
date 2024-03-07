89a047efc519ed219bc6f026ee53d5cf2683248d
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-03-07 09:55:29 +0100

    .

--- firts-react-course/src/Components/FavoriteCar.jsx
+++ firts-react-course/src/Components/FavoriteCar.jsx
@@ -5,9 +5,15 @@ const FavoriteCar = () => {
         = useState(
             {name: 'Ford', year: 2024, model: 'Mustang'})
 
+    
+
     return (
         <div>
             <p>Your favorite car is: {car.year} {car.name} {car.model}</p>
+
+            <input type="number" value={car.year}/> <br/>
+            <input type="text" value={car.name}/> <br/>
+            <input type="text" value={car.model}/>
         </div>
     )
 }
