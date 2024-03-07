53782fb9938a68a322d4c4b5bdcc223d49473a74
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-03-07 10:06:48 +0100

    .

--- firts-react-course/src/Components/FavoriteCar.jsx
+++ firts-react-course/src/Components/FavoriteCar.jsx
@@ -9,8 +9,8 @@ const FavoriteCar = () => {
         setCar(c => ({...c, year: e.target.value}))
     }
 
-    const handleMakeChange = () => {
-
+    const handleMakeChange = (e) => {
+        setCar(c => ({...c, make: e.target.value}))
     }
 
     const handleModelChange = () => {
@@ -22,7 +22,7 @@ const FavoriteCar = () => {
             <p>Your favorite car is: {car.year} {car.make} {car.model}</p>
 
             <input type="number" value={car.year} onChange={handleYearChange}/> <br/>
-            <input type="text" value={car.make}/> <br/>
+            <input type="text" value={car.make} onChange={handleMakeChange}/> <br/>
             <input type="text" value={car.model}/>
         </div>
     )
