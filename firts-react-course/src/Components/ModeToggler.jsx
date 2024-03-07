40eca7db552f64ea15c36cf36fab4b23c69bd54e
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-03-07 06:39:01 +0100

    .

--- firts-react-course/src/Components/ModeToggler.jsx
+++ firts-react-course/src/Components/ModeToggler.jsx
@@ -1,19 +1,19 @@
 import {useState} from "react";
 
 function ModeToggle() {
-    let darkModeOn = true
-    const darkMode = <h1>Dark mode is On</h1>
-    const lightMode = <h1>Light mode is On</h1>
+    // let darkModeOn = true
+    // const darkMode = <h1>Dark mode is On</h1>
+    // const lightMode = <h1>Light mode is On</h1>
     const [name, setName] = useState('')
     
-    function handler() {
-        darkModeOn = !darkModeOn
-        if (darkModeOn === true) {
-            console.log('Dark Mode is On')
-        } else {
-            console.log('Light Mode is On')
-        }
-    }
+    // function handler() {
+    //     darkModeOn = !darkModeOn
+    //     if (darkModeOn === true) {
+    //         console.log('Dark Mode is On')
+    //     } else {
+    //         console.log('Light Mode is On')
+    //     }
+    // }
 
     const handleNameChange = (event) => {
         setName(event.target.value)
@@ -21,10 +21,10 @@ function ModeToggle() {
 
     return (
         <div>
-            {darkModeOn ? darkMode : lightMode}
-            <button className={'button'} onClick={handler}>
-                Click me
-            </button>
+            {/*{darkModeOn ? darkMode : lightMode}*/}
+            {/*<button className={'button'} onClick={handler}>*/}
+            {/*    Click me*/}
+            {/*</button>*/}
 
             <input value={name} onChange={handleNameChange}/>
             <p>Name: {name}</p>
