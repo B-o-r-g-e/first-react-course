334845f255236e9465a225b44734977d76f6950d
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-03-08 00:37:14 +0100

    .

--- firts-react-course/src/Components/FavCarAnother.jsx
+++ firts-react-course/src/Components/FavCarAnother.jsx
@@ -30,6 +30,11 @@ function FavCarAnother () {
     return (
         <div>
             <h2>List of Car Objects</h2>
+            <ul>
+                {cars.map((car, index) => <li key={index}>
+                    {car.year} {car.make} {car.model}
+                </li>)}
+            </ul>
             <input type="number" value={carYear} onChange={handleYearChange}/>
             <input type="text" value={carMake} onChange={handleMakeChange} placeholder={'Enter Car Make'}/>
             <input type="text" value={carModel} onChange={handleModelChange} placeholder={'Enter Car Model'}/>
