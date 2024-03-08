840696f419c0054d4957196d7e887f95a45fe0b6
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-03-08 05:43:26 +0100

    .

--- firts-react-course/src/Components/ToDoList.jsx
+++ firts-react-course/src/Components/ToDoList.jsx
@@ -3,12 +3,19 @@ import {useState} from "react";
 function ToDoList() {
     const [newTask, setNewTask] = useState('')
 
+    function handleInputChnage() {
+
+    }
 
     return (
         <div className="to-do-list">
             <h1>To-Do-List</h1>
             <div>
-                <input type="text" placeholder={'Enter a task'} value={newTask}/>
+                <input type="text"
+                       placeholder={'Enter a task'}
+                       value={newTask}
+                       onChange={handleInputChnage}
+                />
             </div>
         </div>
     )
