0a30da5b87b14faa2b67624f5d733872b84342ed
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-03-12 00:17:33 +0100

    .

--- firts-react-course/src/Components/DigitalClock.jsx
+++ firts-react-course/src/Components/DigitalClock.jsx
@@ -1,10 +1,33 @@
-const DigitalClock = () => {
+import {useEffect, useState} from "react";
 
+const DigitalClock = () => {
+    const [time, setTime] = useState(new Date())
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setTime(new Date())
+        }, 1000)
+
+        return () => {
+            clearInterval(intervalId)
+        }
+    }, []);
+    
+    function formatTime() {
+        let hours = time.getHours()
+        const minute = time.getMinutes()
+        const seconds = time.getSeconds()
+        const meridiem = hours >= 12 ? 'PM' : 'AM'
+
+        hours = hours % 12 || 12
+
+        return `${hours}:${minute}:${seconds} ${meridiem}`
+    }
 
     return (
         <div className={'clock-container'}>
             <div className="clock">
-                <span>00:00:00</span>
+                <span>{formatTime()}</span>
             </div>
         </div>
     )
