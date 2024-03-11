ad8337d640372b682234d7a0ef2c3bb017aa65ea
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-03-11 23:17:31 +0100

    .

--- firts-react-course/src/Components/FirstEffect.jsx
+++ firts-react-course/src/Components/FirstEffect.jsx
@@ -2,19 +2,31 @@ import {useEffect, useState} from "react";
 
 function FirstEffect() {
     const [count, setCount] = useState(0)
+    const [color, setColor] = useState('green')
 
     useEffect(() => {
         document.title = `Count: ${count}`
+        // document.title = `My Counter Program`
     });
 
     function addCount() {
         setCount(c => c + 1)
     }
 
+    function subtractCount() {
+        setCount(c => c - 1)
+    }
+
+    function changeColor() {
+        setColor(c => c === 'green' ? 'red' : 'green')
+    }
+
     return (
         <div>
             <p>Count: {count}</p>
             <button onClick={addCount}>Add</button>
+            <button onClick={subtractCount}>Subtract</button><br/>
+            <button onClick={changeColor}>Change Color</button>
         </div>
 
     )
