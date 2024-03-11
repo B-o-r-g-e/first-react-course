beb08774d8324ef2953f167ba512ffa525bdf4af
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-03-11 23:31:02 +0100

    .

--- firts-react-course/src/Components/WidthHeight.jsx
+++ firts-react-course/src/Components/WidthHeight.jsx
@@ -1,5 +1,20 @@
-const WidthHeight = () => {
+import {useState} from "react";
 
+const WidthHeight = () => {
+    const [width, setWidth] = useState(window.innerWidth)
+    const [height, setHeight] = useState(window.innerHeight)
+
+    function handleResize(){
+        setWidth(window.innerWidth)
+        setHeight(window.innerHeight)
+    }
+
+    return (
+        <div>
+            <p>Window Width: {width}px</p>
+            <p>Window Height: {height}px</p>
+        </div>
+    )
 }
 
 export default WidthHeight
