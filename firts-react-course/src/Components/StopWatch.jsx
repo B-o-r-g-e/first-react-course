64c245ca3fea16244559ac384fad2e2787fe77d7
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-03-12 03:30:10 +0100

    .

--- firts-react-course/src/Components/StopWatch.jsx
+++ firts-react-course/src/Components/StopWatch.jsx
@@ -1,4 +1,4 @@
-import {useRef, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 
 function StopWatch() {
     const [isRunning, setIsRunning] = useState(false)
@@ -6,9 +6,34 @@ function StopWatch() {
     const intervalIdRef = useRef();
     const startTimeRef = useRef();
 
-    return (
-        <div>
+    useEffect(() => {
 
+    }, [isRunning]);
+    
+    function start() {
+        
+    }
+    
+    function stop() {
+        
+    }
+    
+    function reset() {
+        
+    }
+    
+    function formatTime() {
+        return `00:00:00`
+    }
+
+    return (
+        <div className={'stop-watch'}>
+            <div className="display">{formatTime()}</div>
+            <div className="controls">
+                <button className="start-button" onClick={start}>Start</button>
+                <button className="stop-button" onClick={stop}>Stop</button>
+                <button className="reset-button" onClick={reset}>Reset</button>
+            </div>
         </div>
     )
 }
