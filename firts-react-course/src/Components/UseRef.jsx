25314c42229a296b16ca60a4a889b0c5853cd96d
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-03-12 02:13:58 +0100

    .

--- firts-react-course/src/Components/UseRef.jsx
+++ firts-react-course/src/Components/UseRef.jsx
@@ -1,15 +1,44 @@
 import {useRef} from "react";
 
 function UseRef() {
-    const ref = useRef(0);
+    const inputRef1 = useRef(null);
+    const inputRef2 = useRef(null);
+    const inputRef3 = useRef(null);
 
-    function handleClick() {
-        ref.current++
-        console.log(ref.current)
+    function handleClick1() {
+        inputRef1.current.focus()
+        inputRef1.current.style.backgroundColor = 'yellow'
+        inputRef2.current.style.backgroundColor = ''
+        inputRef3.current.style.backgroundColor = ''
+    }
+
+    function handleClick2() {
+        inputRef2.current.focus()
+        inputRef2.current.style.backgroundColor = 'yellow'
+        inputRef3.current.style.backgroundColor = ''
+        inputRef1.current.style.backgroundColor = ''
+    }
+
+    function handleClick3() {
+        inputRef3.current.focus()
+        inputRef3.current.style.backgroundColor = 'yellow'
+        inputRef2.current.style.backgroundColor = ''
+        inputRef1.current.style.backgroundColor = ''
     }
 
     return (
-        <button onClick={handleClick}>Click me</button>
+        <div>
+            <button onClick={handleClick1}>Click me 1</button>
+            <input ref={inputRef1}/>
+            <br/>
+
+            <button onClick={handleClick2}>Click me 2</button>
+            <input ref={inputRef2}/>
+            <br/>
+
+            <button onClick={handleClick3}>Click me 3</button>
+            <input ref={inputRef3}/>
+        </div>
     )
 }
 
