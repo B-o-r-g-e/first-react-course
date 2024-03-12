9f69711dfe7f0163dc1aa93ecceca15f7c25c1ce
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-03-12 01:26:43 +0100

    .

--- firts-react-course/src/Components/ComponentC.jsx
+++ firts-react-course/src/Components/ComponentC.jsx
@@ -1,10 +1,15 @@
 import ComponentD from "./ComponentD.jsx";
+import {useContext} from "react";
+import {UserContext} from "./ComponentA.jsx";
 
 function ComponentC() {
+
+    const user = useContext(UserContext)
 
     return (
         <div className="box">
             <h1>Component C</h1>
+            <h2>{user}</h2>
             <ComponentD />
         </div>
     )
