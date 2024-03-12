2bc5dc2e7e74c725163594b0974f1f51a1b38985
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-03-12 02:46:38 +0100

    .

--- firts-react-course/src/Components/StopWatch.jsx
+++ firts-react-course/src/Components/StopWatch.jsx
@@ -1,4 +1,10 @@
+import {useRef, useState} from "react";
+
 function StopWatch() {
+    const [isRunning, setIsRunning] = useState(false)
+    const [elapsedTime, setElapsedTime] = useState()
+    const intervalIdRef = useRef();
+    const startTimeRef = useRef();
 
     return (
         <div>
